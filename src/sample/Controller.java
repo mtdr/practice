44c735a57e0492f235e9d7e6b6a31f91a9e547a8
Julia Polushina <287jpi@gmail.com>
2016-06-24 22:50:33 +0300

    1.10
    Добавлено ограничение на ввод в некоторые текстовые поля

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML
+    private TextField graphX;
+    @FXML
+    private TextField graphY;
+    @FXML
+    private TextField vertexGraph1;
+    @FXML
+    private TextField vertexGraph2;
+    private Alert alert;
+    @FXML
+    public void handleAddGraph () {
+        if (graphX.getText() == null || graphX.getText().length() == 0) {
+            error("Пожалуйста, заполните поле X");
+        }
+        else {
+            if (graphY.getText() == null || graphY.getText().length() == 0) {
+                error("Пожалуйста, заполните поле Y");
+            }
+            else {
+                try {
+                    int x = Integer.parseInt(graphX.getText());
+                    int y = Integer.parseInt(graphY.getText());
+                    if (x > 15 || x < 0 || y > 10 || y < 0) {
+                        error("Граф может быть не отображен. Пожалуйста, введите координаты в соответствии заданной координатной осью.");
+                    }
+                    // и вот тут наконец-то начинается магия с созданием графов
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                    error("Введено некорректное значение в одно из полей! Пожалуста, вводите только цифры.");
+                }
+            }
+        }
+    }
+    @FXML
+    public void handleAddEdge () {}
+
+    public void error(String s) {
+        alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Некорректный ввод");
+        alert.setHeaderText(s);
+        alert.showAndWait();
+    }
 }
